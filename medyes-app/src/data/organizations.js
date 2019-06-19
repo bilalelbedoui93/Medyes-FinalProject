d6d69e26a33a9dc55020855385f8b56381ee8042
unknown <bilalelbedoui93@gmail.com>
2019-06-19 15:34:34 +0200

    first commit

---
+++ medyes-app/src/data/organizations.js
@@ -0,0 +1,21 @@
+import call from '../common/call'
+
+
+
+const organizations = {
+
+    __url__: 'https://floating-refuge-24678.herokuapp.com/api/organization',
+
+    addOrganization(token, name, phone, address, mail) {
+        return (async()=>{
+            return await call(`${this.__url__}`, {
+                method: 'POST',
+                headers: { 'x-auth-token': token },
+                data: { name, phone, address, mail }
+            })
+        })()
+    }
+}
+
+
+export default organizations
